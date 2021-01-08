b4ba036f85884452a0fa3eb6499fa1c556ce0bfb
MaksymBukshovan <mbukshovan@danavero.com>
2021-01-08 21:15:34 +0200

    Added XCFramework

---
+++ OkCollect.xcframework/ios-arm64_x86_64-simulator/OkCollect.framework/Headers/OkCollect.h
@@ -0,0 +1,18 @@
+//
+//  OkCollect.h
+//  OkCollect
+//
+//  Created by Maxim on 23.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OkCollect.
+FOUNDATION_EXPORT double OkCollectVersionNumber;
+
+//! Project version string for OkCollect.
+FOUNDATION_EXPORT const unsigned char OkCollectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OkCollect/PublicHeader.h>
+
+
